5a0635d336be756fd45280d217c63364f47cd692
Kiran Babu <kiranbabu3355@gmail.com>
2023-12-23 15:41:43 +0530

    Vision added

---
+++ src/pages/Vision.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Vision.css";
+
+const Vision = () => {
+  return (
+    <>
+      <div className="vis-main">
+        <div className="vis-head">Our Objectives</div>
+      </div>
+    </>
+  );
+};
+
+export default Vision;
