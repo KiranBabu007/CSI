085b0d7e03103929db9e82ed96cc69473f913225
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-12-24 20:34:44 +0530

    Update NavBar component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from './pages/Home/Home';
 import './index.css';
-import NavBar from "./components/Navbar/NavBar";
+import NavBar from "./components/Navbar/Nav";
 
 function App() {
   const router = createBrowserRouter([
