48b4e5ea6fad9f6c077966ccf9dcea3e5a24e2c7
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-12-24 08:59:29 +0530

    Update logo and navbar styles

--- src/Components/navbar/Nav.jsx
+++ src/Components/navbar/Nav.jsx
@@ -1,8 +1,26 @@
 import { Outlet } from "react-router-dom";
+import styles from "./nav.module.css";
 const Nav = () => {
     return (
         <div>
-            NAV
+            <div className={styles.navBar}>
+                <div className={styles.logoSection}>
+                    <p className={styles.logo}></p>
+                    <hr />
+                    <div className={styles.logoText}>
+                        <h1>SJCET PALAI</h1>
+                        <p>COMPUTER SOCIETY <br /> OF INDIA</p>
+                    </div>
+                </div>
+                <div className={styles.menuSection}>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Events</p>
+                    <p>Team</p>
+                    <p>Gallery</p>
+                    <p>Contact</p>
+                </div>
+            </div>
             <Outlet />
         </div>
     )
--- src/modules/hero/Hero.jsx
+++ src/modules/hero/Hero.jsx
@@ -1,4 +1,8 @@
 import styles from './Hero.module.css'
+import bg1 from '../../assets/images/events1.png'
+import bg2 from '../../assets/images/events2.png'
+import bg3 from '../../assets/images/events3.png'
+
 const Hero = () => {
     return (
         <div className={styles.hero}>
@@ -11,15 +15,12 @@ const Hero = () => {
                     <hr />
                 </div>
 
-                <div className={styles.events}>
-                    <div className={styles.card}>
-
-                    </div>
-                    <div className={styles.card}>
-
-                    </div>
-                    <div className={styles.card}>
-
+                <div className={styles.eventContainer}>
+                    <div className={styles.events}>
+                        <div style={{ backgroundImage: `url(${bg1})` }} className={styles.card}></div>
+                        <div style={{ backgroundImage: `url(${bg2})` }} className={styles.card}></div>
+                        <div style={{ backgroundImage: `url(${bg3})` }} className={styles.card}></div>
+                        <div style={{ backgroundImage: `url(${bg3})` }} className={styles.card}></div>
                     </div>
                 </div>
             </div>
