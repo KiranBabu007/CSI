987844dc5a0151663c43b3f42cb2e4e119c3a638
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-12-31 18:58:03 +0530

    Update code: Removed unused imports and commented out code

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
 import styles from './NavBar.module.css'
 import { Outlet } from "react-router-dom";
 
 const NavBar = () => {
+    const [sideNavDisplay, setSideNavDisplay] = useState(
+        window.innerWidth > 830 ? "flex" : "none"
+    );
+    const [marginTop, setMarginTop] = useState("0px");
+    const [transform2, setTransform2] = useState("0deg");
+    const [transform3, setTransform3] = useState("0deg");
+    const [hamburgerIconDisplay, setHamburgerIconDisplay] = useState("block");
+
+    useEffect(() => {
+        const handleResize = () =>
+            setSideNavDisplay(window.innerWidth > 830 ? "flex" : "none");
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const animateHamburgerIcon = () => {
+        setMarginTop(hamburgerIconDisplay === "none" ? "0px" : "-14.5px");
+        setTransform2(hamburgerIconDisplay === "none" ? "0deg" : "135deg");
+        setTransform3(hamburgerIconDisplay === "none" ? "0deg" : "45deg");
+        setHamburgerIconDisplay(hamburgerIconDisplay === "none" ? "block" : "none");
+    };
+
+    const toggleSideNavBar = () => {
+        animateHamburgerIcon();
+        setSideNavDisplay(sideNavDisplay === "none" ? "flex" : "none");
+
+    };
+
     return (
         <div>
             <div className={styles.navBar}>
@@ -13,7 +43,7 @@ const NavBar = () => {
                         <p>COMPUTER SOCIETY <br /> OF INDIA</p>
                     </div>
                 </div>
-                <div className={styles.menuSection}>
+                <div className={styles.menuSection} style={{ display: `${sideNavDisplay}` }}>
                     <p>Home</p>
                     <p>About</p>
                     <p>Events</p>
@@ -21,10 +51,16 @@ const NavBar = () => {
                     <p>Gallery</p>
                     <p>Contact</p>
                 </div>
+            {/* Hamburger Menu */}
+            <div className={styles.navButton} onClick={toggleSideNavBar}>
+                <div style={{ transform: `rotate(${transform2})`, marginBottom: `${marginTop}` }} className={styles.navButton1}></div>
+                <div style={{ transform: `rotate(${transform3})` }} className={styles.navButton2}></div>
+                <div style={{ display: `${hamburgerIconDisplay}` }} className={styles.navButton3}></div>
+            </div>
             </div>
             <Outlet />
         </div>
     )
 }
 
-export default NavBar
+export default NavBar;
