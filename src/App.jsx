a9fa75f2910be56a47c9499ef953d0ca8e0c0023
Kiran Babu <kiranbabu3355@gmail.com>
2024-02-06 23:38:56 +0530

    Add Team page to navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./index.css";
 import NavBar from "./Components/NavBar/NavBar";
+import Team from "./pages/Team/Team";
 
 function App() {
   const router = createBrowserRouter([
@@ -10,10 +11,14 @@ function App() {
       element: <NavBar />,
       children: [{ path: "", element: <Home /> }],
     },
+    {
+      path: "/team",
+      element: <NavBar />,
+      children: [{ path: "", element: <Team /> }],
+    },
   ]);
 
   return <RouterProvider router={router} />;
 }
-
 
 export default App;
---
+++ src/pages/Team/Team.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Team.css";
+
+const Team = () => {
+  return <div>Teams</div>;
+};
+
+export default Team;
