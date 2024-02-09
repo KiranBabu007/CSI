9878e20b0b0be965c9ff702a05dddcea8d723a5c
Kiran Babu <kiranbabu3355@gmail.com>
2024-02-09 21:07:42 +0530

    Add Team navigation and header box

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Team.css";
 
 const Team = () => {
-  return <div>Teams</div>;
+  return (
+    <>
+      <div className="Team-Nav"></div>
+      <div className="Team-main">
+        <div className="Team-header-box">
+        <div className="Team-subhead">
+          <div className="Team-subhead-box">WHO</div>
+          </div>
+      </div>
+      </div>
+      
+    </>
+  );
 };
 
 export default Team;
